39f66845b82ce09e652632d668b80e85ae55f57b
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-01 11:44:23 +0200

    random

--- javascript/flashcard.js
+++ javascript/flashcard.js
@@ -1,6 +1,6 @@
 
 
 var words = ["Boek", "Hoi", "Hoe gaat het?"];
-var randomWords = words[Math.floor(Math.random() * words.length)];
-
+// var randomWords = words[Math.floor(Math.random() * words.length)];
+var randomWords = words[Math.floor(Math.random() * words.length)]
 console.log(randomWords);
