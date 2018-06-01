64e2046b2369bd38d169afd19c7d8a11dcc03662
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-06-01 16:03:56 +0200

    try

--- javascript/flashcard.js
+++ javascript/flashcard.js
@@ -1,9 +1,8 @@
 
 
+
 var DutchWords = [{ question : "Boek", answer : "Livro"}, {question: "Hoi", answer: "Ola"}, {question : "Hoe gaat het?", answer : "Oi como esta?"}];
 var indexNumber = Math.floor(Math.random() * DutchWords.length)
-
-
 
 var randomWords = DutchWords[indexNumber].question
 // var randomWords = DutchWords[Math.floor(Math.random() * DutchWords.length)].question
@@ -11,15 +10,14 @@ var randomAnswer = DutchWords[indexNumber].answer
 
 var points = 0;
 
-console.log(DutchWords[0].question);
-console.log(randomWords);
-console.log(`this is randomanswer ${randomAnswer}`)
+// console.log(DutchWords[0].question);
+// console.log(randomWords);
+// console.log(`this is randomanswer ${randomAnswer}`)
 
 function showRandomWord(){
     document.getElementById('randomword').innerHTML = randomWords;
 }
 console.log(showRandomWord())
-
 
 function submitEvent(){
   document.getElementById('randomword').removeAttribute = '';
@@ -27,29 +25,33 @@ function submitEvent(){
   console.log(randomWords)
 
 
-var inputAnswer = document.getElementById('input').value;
-console.log(inputAnswer)
+  var inputAnswer = document.getElementById('input').value;
+  console.log(inputAnswer)
 
-// make randomAnswer ignore case
-var inputA = inputAnswer.toLowerCase()
-var answerB = randomAnswer.toLowerCase()
+  // make randomAnswer ignore case
+  var inputA = inputAnswer.toLowerCase()
+  var answerB = randomAnswer.toLowerCase()
 
-  if (inputA === answerB){
-  alert('Yes!!!!')
-  // var points = document.getElementById('pointBox').innerHTML = points++
+    if (inputA === answerB){
+    alert('Yes!!!!')
+    // var points = document.getElementById('pointBox').innerHTML = points++
+      points++
+    document.getElementById('pointBox').innerHTML = `1 extra point!`
 
-  (document.getElementById('pointBox').innerHTML = `Points: ${points++}`)
-
-  } else {
-    alert('try again')
-  }
+    } else {
+      alert('try again')
+    }
 }
 
 function nextEvent(){
+// showRandomWord();
+
   location.reload();
+//when we submit we want to go to the next questio without refreshing the page.
+  //next // QUESTion
 
 }
-  (document.getElementById('pointBox').innerHTML = `Points: ${points++}`)
+
 
 var inputAnswer = document.getElementById('input').value;
 console.log(inputAnswer)
